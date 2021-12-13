6d10bf236b00d9530a923af1a78d0910f35dd8cc
youdaihoi <yohdai.camel@gmail.com>
2021-12-13 09:35:48 +0000

    Appli2-2...4

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '245c22af689dd3932031cf308f4a268e0109478227e41f1d06618930a18a8a833b672b923009010f4a57b248be317f5cd678db0e49c7af74f5b0d92c2639ebf7'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    
+    resources :post_comments, only: [:create, :destroy]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
